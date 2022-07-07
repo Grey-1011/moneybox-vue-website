f458a236f5e649babca919de9563bea924950e7d
grey <z1251543107@gmail.com>
2022-07-07 16:44:41 +0800

    deploy

---
+++ precache-manifest.0896a13fd6f8e69a1a9ffb47c329816a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371995da9ba3012bc4f7",
+    "url": "/moneybox-vue-website/css/app.e533e1e3.css"
+  },
+  {
+    "revision": "e6df1482b46fac9da682",
+    "url": "/moneybox-vue-website/css/chunk-vendors.054aa86a.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/moneybox-vue-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/moneybox-vue-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "dd71f6d0f6614e0dff4d6d23192a6fa2",
+    "url": "/moneybox-vue-website/index.html"
+  },
+  {
+    "revision": "371995da9ba3012bc4f7",
+    "url": "/moneybox-vue-website/js/app.d46f9689.js"
+  },
+  {
+    "revision": "e6df1482b46fac9da682",
+    "url": "/moneybox-vue-website/js/chunk-vendors.d6895fa3.js"
+  },
+  {
+    "revision": "e4fc451d6b85f9ee065a7c8e29d3d71a",
+    "url": "/moneybox-vue-website/manifest.json"
+  },
+  {
+    "revision": "90eae343c0a451972058d3c6dec0be73",
+    "url": "/moneybox-vue-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneybox-vue-website/robots.txt"
+  }
+]);
